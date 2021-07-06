be7e85150766abf4b520acf34c53a6f9b997182f
Mark Bower <mark.bower@yale.edu>
2021-07-06 11:36:08 -0600

    Renamed everything to buckets

---
+++ Analysis/buckets/R/buckets.R
@@ -0,0 +1,92 @@
+buckets <- function( accumulatorSize, FUN, recipient, primeSize=0 ) {
+  #
+  # accumulatorSize: the number of events held until you do something
+  # FUN            : what is done with a full accumulator
+  # recipient      : where to send the result
+  # primeSize      : the number of FUN runs to be completed before broadcasting
+  #
+  #' @export
+  #' @examples
+  library(future)
+
+  accumulator <- vector( mode='double', length=accumulatorSize )
+  accumulatorCount <- 0
+  stack <- NULL
+  writeCount  <- 0
+  primeCnt <- 0
+  sendCount <- 0
+  readCount <- 0
+  
+  prime <- function( events ) { # do not call the function
+    for ( idx in seq(1,length(events) ) ) {
+      add_single( events[idx], flag=FALSE )
+    }
+  }
+  
+  add <- function( events ) {
+    # Is event a scalar or vector?
+    for ( idx in seq(1,length(events) ) ) {
+      add_single( events[idx] )
+    }
+  }
+  
+  add_single <- function( event, flag=TRUE ) {
+    # event is a scalar
+    accumulatorCount <<- accumulatorCount + 1
+    accumulator[[accumulatorCount]] <<- event
+    if ( accumulatorCount == accumulatorSize ) {
+      primeCnt <<- primeCnt + 1
+      writeCount <<- writeCount + 1
+      if ( !is.null(FUN) ) {
+        stack[[writeCount]] <<- future( FUN( accumulator[1:accumulatorCount] ) )
+      } else {
+        stack[[writeCount]] <<- future( accumulator[1:accumulatorCount] )
+      }
+      # Send forward?
+      if ( primeCnt >= primeSize ) {
+        sendCount <<- sendCount + 1
+        if ( !is.null(recipient) ) {
+          recipient$add( value(stack[[sendCount]]) )
+        }
+      }
+      accumulatorCount <<- 0
+    }
+  }
+  
+  getValue <- function() {
+    readCount <<- readCount + 1
+    if ( readCount <= length(stack) ) {
+      returnValue <- value(stack[[readCount]])
+      return( returnValue )
+    } else {
+      readCount <<- readCount - 1
+      return( NULL )
+    }
+  }
+  
+  flush <- function() {
+    # Check for an empty accumulator
+    if ( accumulatorCount == 0 ) {
+      return(NULL)
+    }
+    # Flush the accumulator to the stack
+    writeCount <<- writeCount + 1
+    if ( !is.null(FUN) ) {
+      stack[[writeCount]] <<- future( FUN( accumulator[1:accumulatorCount] ) )
+    } else {
+      stack[[writeCount]] <<- future( accumulator[1:accumulatorCount] )
+    }
+    accumulatorCount <<- 0
+    # Flush the stack to the receiver
+    while ( sendCount < length(stack) ) {
+      sendCount <<- sendCount + 1
+      if ( !is.null(recipient) ) {
+        recipient$add( value(stack[[sendCount]]) )        
+      }
+    }
+  }
+
+  obj <- list(prime=prime,add=add,getValue=getValue,flush=flush)
+  class(obj) <- c('buckets' )
+  return( obj )
+}
---
+++ Analysis/buckets/tests/testthat/test-eventsBufferFutures.R
@@ -0,0 +1,111 @@
+library(buckets)
+
+test_that("buffering, function call and value works", {
+  bucket <- buckets( accumulatorSize=10, mean, NULL )
+  for ( i in seq(1,100) ) {bucket$add( i )}
+  expect_equal( bucket$getValue(), 5.5 )
+})
+
+test_that("reading through buffered results works",{
+  bucket <- buckets( accumulatorSize=10, mean, NULL )
+  for ( i in seq(1,100) ) {bucket$add( i )}
+  for ( i in seq(1,9) ) {bucket$getValue()}
+  expect_equal( bucket$getValue(), 95.5 )
+})
+
+test_that("composite functions work", {
+  sumOfSquares <- function(x) {sum( x*x )}
+  bucket <- buckets( accumulatorSize=10, sumOfSquares, NULL )
+  for ( i in seq(1,100) ) {bucket$add( i )}
+  expect_equal( bucket$getValue(), 385 )
+})
+
+test_that("flush works",{
+  bucket <- buckets( accumulatorSize=10, mean, NULL )
+  for ( i in seq(1,103) ) {bucket$add( i )}
+  bucket$flush()
+  for ( i in seq(1,10) ) {bucket$getValue()}
+  expect_equal( bucket$getValue(), 102 )
+})
+
+test_that("super passing raw data to sub works",{
+  # This tortured example also tests whether one bucket can pass unaltered arguments
+  # to subsequent buckets when the "function" of the "super" bucket is NULL.
+  bucket_sub <- buckets( accumulatorSize=10, mean, NULL )
+  bucket_super <- buckets( accumulatorSize=5, NULL, bucket_sub )
+  for ( i in seq(1,18) ) {bucket_super$add(i)}
+  expect_equal( bucket_sub$getValue(), 5.5 )
+})
+
+test_that("sub flush works",{
+  bucket_sub <- buckets( accumulatorSize=10, mean, NULL )
+  bucket_super <- buckets( accumulatorSize=5, NULL, bucket_sub )
+  for ( i in seq(1,18) ) {bucket_super$add(i)}
+  bucket_sub$getValue()
+  bucket_sub$flush()
+  expect_equal( bucket_sub$getValue(), 13 )
+})
+
+test_that("super/sub flush interaction works",{
+  bucket_sub <- buckets( accumulatorSize=10, mean, NULL )
+  bucket_super <- buckets( accumulatorSize=5, NULL, bucket_sub )
+  for ( i in seq(1,18) ) {bucket_super$add(i)}
+  bucket_sub$getValue()
+  bucket_sub$flush()
+  bucket_sub$getValue()
+  bucket_super$flush()
+  bucket_sub$flush()
+  expect_equal( bucket_sub$getValue(), 17 )
+})
+
+test_that("priming works",{
+  bucket <- buckets( accumulatorSize=10, mean, NULL, prime=5 )
+  for ( i in seq(1,15) ) {bucket$add( i )}
+  # Only 1 item should be on the stack.
+  expect_equal( bucket$getValue(), 5.5 )
+  # Subsequent calls fail
+  testthat::expect_null( ( bucket$getValue() ) )
+  # Flush
+  bucket$flush()
+  # Now "get" succeeds
+  expect_equal( bucket$getValue(), 13 )
+})
+
+test_that("priming before a send works",{
+  bucket_sub <- buckets( accumulatorSize=10, mean, NULL )
+  bucket_super <- buckets( accumulatorSize=10, NULL, bucket_sub, prime=5 )
+  for ( i in seq(1,49) ) {bucket_super$add(i)}
+  # "_super" should have some results, but ...
+  expect_equal( length( bucket_super$getValue() ), 10 )
+  expect_equal( mean( bucket_super$getValue() ), 15.5 )
+  # ... "sub" shouldn't have a single entry.
+  testthat::expect_null( ( bucket_sub$getValue() ) )
+  # Flushing "sub" should not help
+  bucket_sub$flush()  
+  testthat::expect_null( ( bucket_sub$getValue() ) )
+  # Flushing "super" should send the first element to "sub"
+  bucket_super$flush()
+  expect_equal( mean( bucket_sub$getValue() ), 5.5 )
+})
+
+test_that("priming too short still works",{
+  bucket_sub <- buckets( accumulatorSize=10, mean, NULL )
+  bucket_super <- buckets( accumulatorSize=10, NULL, bucket_sub, prime=1 )
+  for ( i in seq(1,100) ) {bucket_super$add(i)}
+  for ( j in seq(1,5) ) {bucket_sub$getValue()}
+  expect_equal( bucket_sub$getValue(), 55.5 ) 
+})
+
+test_that("priming with multiple levels works",{
+  bucket_3 <- buckets( accumulatorSize=10, mean, NULL )
+  bucket_2 <- buckets( accumulatorSize=10, NULL, bucket_3 )
+  bucket_1 <- buckets( accumulatorSize=10, NULL, bucket_2, prime=5 )
+  for ( i in seq(1,49) ) {bucket_1$add(i)}
+  # If the first buffer is not fully primed, then nothing is sent.
+  testthat::expect_null( ( bucket_3$getValue() ) )
+  # Once it is fully primed, the processing chain is activated.
+  bucket_1$add(50)
+  expect_equal( mean( bucket_1$getValue() ), 5.5 )
+})
+
+
